662f4dd490861240a5ef9b2c5b174571ed6e78c1
Pavel Grechishny <grechishny.p@mail.ru>
2023-09-19 09:30:53 +0300

    подключение: модуль players.py

--- temp.py
+++ temp.py
@@ -1,34 +1,7 @@
-from configparser import ConfigParser
-from pathlib import Path
-from sys import path
-
 import menu
+import players
 
-# Функции для работы с файлом игроков players.ini
-def create_player(player_name: str):
-    """Функция добавления нового игрока в файл players.ini"""
-    player = ConfigParser()
-    player.add_section(player_name)
-    keys = ('wins', 'draws', 'loses')
-    for key in keys:
-        player.set(player_name, key, '0')
-    file = Path(path[0]) / 'players.ini'
-    with open(file, 'a', encoding='utf-8') as fileout:
-        player.write(fileout)
 
-def read_players() -> dict:
-    """Функция чтения файла players.ini"""
-    players = ConfigParser()
-    players.read('players.ini')
-    return players
-
-def update_player(player_name, key, value):
-    """Функция обновления данных в файле players.ini"""
-    players = read_players()
-    players.set(player_name, key, value)
-    file = Path(path[0]) / 'players.ini'
-    with open(file, 'w', encoding='utf-8') as fileout:
-        players.write(fileout)
         
 # Функция шаблона игрового поля
 def create_play_field(size: int=3) -> str:
@@ -47,20 +20,16 @@ def create_play_field(size: int=3) -> str:
 # Функция построения выигрышных комбинаций
 def create_wins(size: int=3) -> list[set[int]]:
     """Функция возвращает список сетов выигрышных комбинаций в зависимости от размера игрового поля."""
+    
+    # Целевой список победных комбинаций
     wins_combinations = []
+    # Список всех номеров игрового поля
     all_numbers = [i+1 for i in range(size**2)]
-
     # Для сбора комбинаций используется двумерный список
     main_list = [all_numbers[i:i+size] for i in range(0, len(all_numbers), size)]
-    
-    
-
-        
-        
-   
-    # Победная комбинация по главной диагонали
-    # Победная комбинация по обратной диагонали    
+    # Победная комбинация по главной диагонали 
     diagonal_main = [] 
+    # Победная комбинация по обратной диагонали
     diagonal_back = []    
     
     for i in range(size):
@@ -84,9 +53,16 @@ def create_wins(size: int=3) -> list[set[int]]:
     
 # >>> create_wins(4)
 # [
-    # {1, 2, 3, 4}, {1, 13, 5, 9}, {8, 5, 6, 7}, 
-    # {2, 10, 6, 14}, {9, 10, 11, 12}, {11, 3, 15, 7}, 
-    # {16, 13, 14, 15}, {8, 16, 4, 12}, {16, 1, 11, 6}, {10, 4, 13, 7}
+    # {1, 2, 3, 4}, 
+    # {1, 13, 5, 9}, 
+    # {8, 5, 6, 7}, 
+    # {2, 10, 6, 14}, 
+    # {9, 10, 11, 12}, 
+    # {11, 3, 15, 7}, 
+    # {16, 13, 14, 15}, 
+    # {8, 16, 4, 12}, 
+    # {16, 1, 11, 6}, 
+    # {10, 4, 13, 7}
 # ]
 
 
