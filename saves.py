7b4c8c3e5b58e5df4c41303b9d12a4888048cd4d
Pavel Grechishny <grechishny.p@mail.ru>
2023-09-26 13:39:40 +0300

    переименование: parse_to_string

--- saves.py
+++ saves.py
@@ -1,17 +1,7 @@
 from pathlib import Path
 from sys import path
 
-
-db_saves = {
-    ('player_01', 'player_02'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-    ('player_01', 'player_03'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-    ('player_01', 'player_04'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-    ('player_01', 'player_05'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-    ('player_01', 'player_08'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-    ('player_01', 'player_06'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-    ('player_01', 'player_07'): ([1, 2, 4, 3, 9], ['X', '0', '0', 'X', '', '', '', '', 'X']),
-}
-def _parse_ts(obj):
+def parse_to_string(obj):
     """doc"""
     txt_saves = []
     for players, save in obj.items():
@@ -21,18 +11,6 @@ def _parse_ts(obj):
         turns = ','.join(str(t) for t in turns)
         txt_saves.append(f'{players}!{steps}!{turns}')
     return '\n'.join(txt_saves)
- 
-
-save = 'player_01,player_02!1,2,4,3,9!X,0,0,X,,,,,X' 
-# def _parse_fs(obj):
-    # """doc"""
-    # db_saves = {}
-    # players, steps, turns = obj.split('!')
-    # players = tuple(players.split(','))
-    # steps = list(int(s) for s in steps.split(','))
-    # turns = list(turns.split(','))
-    # db_saves[players] = (steps, turns)
-    # return db_saves
 
 def _write_save(obj: str) -> None:
     """doc"""
@@ -40,7 +18,6 @@ def _write_save(obj: str) -> None:
     with open(save_path, 'w', encoding='utf-8') as fileout:
         fileout.write(obj)
            
-    
 def _read_save() -> dict:
     """doc"""
     save_path = Path(path[0]) / 'saves.ttt' 
